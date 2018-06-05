68dee46342006a123ea8d981399ca4c9c6caddb8
ZZFM <flyingman_pan@outlook.com>
2018-06-05 21:48:37 +0800

    add

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthbar;
+	void Start ()
+    {
+        healthbar.value = 1;
+	}
+	void Update ()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        healthbar.value -= 0.0005f;
+	}
+}
---
+++ Assets/Scripts/IMGUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnGUI()
+    {
+        GUI.HorizontalSlider(new Rect(0, 0, 160, 30), 1f,0f,1f);
+    }
+}
